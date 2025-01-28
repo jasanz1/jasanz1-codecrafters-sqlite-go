bdbc218a7cfe13dc3051045ff373b5687c98bd9c
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-27 18:19:45 -0800

    turned database info into struct

--- app/main.go
+++ app/main.go
@@ -10,35 +10,39 @@ import (
 	// "github.com/xwb1989/sqlparser"
 )
 
+type DatabaseFile struct {
+	header   []byte
+	pageSize uint16
+}
+
 // Usage: your_program.sh sample.db .dbinfo
 func main() {
 	databaseFilePath := os.Args[1]
 	command := os.Args[2]
+	database := DatabaseFile{}
+	databaseFile, err := os.Open(databaseFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	database.header = make([]byte, 100)
+
+	_, err = databaseFile.Read(database.header)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := binary.Read(bytes.NewReader(database.header[16:18]), binary.BigEndian, &database.pageSize); err != nil {
+		fmt.Println("Failed to read integer:", err)
+		return
+	}
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 	switch command {
 	case ".dbinfo":
-		databaseFile, err := os.Open(databaseFilePath)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		header := make([]byte, 100)
-
-		_, err = databaseFile.Read(header)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var pageSize uint16
-		if err := binary.Read(bytes.NewReader(header[16:18]), binary.BigEndian, &pageSize); err != nil {
-			fmt.Println("Failed to read integer:", err)
-			return
-		}
-		// You can use print statements as follows for debugging, they'll be visible when running tests.
-		fmt.Fprintln(os.Stderr, "Logs from your program will appear here!")
 
 		// Uncomment this to pass the first stage
-		fmt.Printf("database page size: %v", pageSize)
+		fmt.Printf("database page size: %v", database.pageSize)
 	default:
 		fmt.Println("Unknown command", command)
 		os.Exit(1)
